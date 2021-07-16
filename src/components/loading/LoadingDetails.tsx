1e3f09a20cc5a5dc0dec80a58ce5bc6cb596c9af
Moritz Mock <mock.moritz@rolmail.net>
2021-07-16 09:55:00 +0200

    loading details passes now tone for elements and background

--- src/components/loading/LoadingDetails.tsx
+++ src/components/loading/LoadingDetails.tsx
@@ -9,18 +9,20 @@ import { LoadingField } from '.'
 const LoadingDetails: React.FC<{
   numberOfItems: number
   sectionHeader: React.ReactNode
-}> = ({ numberOfItems, sectionHeader }) => {
+  tone_background?: string
+  tone_elements?: string
+}> = ({ numberOfItems, sectionHeader, tone_background, tone_elements }) => {
   return (
     <Section>
       <SectionHeader>
         <SectionTitle>{sectionHeader}</SectionTitle>
       </SectionHeader>
 
-      <SectionContainerMultiColumn>
+      <SectionContainerMultiColumn tone={tone_background}>
         {Array(numberOfItems)
           .fill(null)
           .map((_, index) => (
-            <LoadingField key={index} />
+            <LoadingField tone={tone_elements} key={index} />
           ))}
       </SectionContainerMultiColumn>
     </Section>
