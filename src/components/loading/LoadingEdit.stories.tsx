d004e6c7da23b9329b048139917047d723ce5997
Moritz Mock <mock.moritz@rolmail.net>
2021-07-16 09:33:32 +0200

    implemented new storybooks for setting colors for LoadingEdit

--- src/components/loading/LoadingEdit.stories.tsx
+++ src/components/loading/LoadingEdit.stories.tsx
@@ -15,5 +15,19 @@ const Template: ComponentStory<typeof LoadingEdit> = (args) => (
 export const Default = Template.bind({})
 Default.args = {
   numberOfItems: 2,
-  sectionHeader: 'I can be changed!',
+  sectionHeader: 'Change me!',
+}
+
+export const ToneBackground = Template.bind({})
+ToneBackground.args = {
+  numberOfItems: 2,
+  sectionHeader: 'Change me!',
+  tone_background: 'critical',
+}
+
+export const ToneElements = Template.bind({})
+ToneElements.args = {
+  numberOfItems: 2,
+  sectionHeader: 'Change me!',
+  tone_elements: 'green',
 }
