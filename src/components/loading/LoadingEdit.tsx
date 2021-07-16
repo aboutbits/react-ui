c3b8e66227d4995bfa8d4dfa4f9b05aaaad18f2d
Moritz Mock <mock.moritz@rolmail.net>
2021-07-16 09:32:53 +0200

    LoadingEdit is passing the colour for the background and inputs downwards

--- src/components/loading/LoadingEdit.tsx
+++ src/components/loading/LoadingEdit.tsx
@@ -10,18 +10,20 @@ import React from 'react'
 const LoadingEdit: React.FC<{
   numberOfItems: number
   sectionHeader: React.ReactNode
-}> = ({ numberOfItems, sectionHeader }) => {
+  tone_background?: string
+  tone_elements?: string
+}> = ({ numberOfItems, sectionHeader, tone_background, tone_elements }) => {
   return (
     <Section>
       <SectionHeader>
         <SectionTitle>{sectionHeader}</SectionTitle>
       </SectionHeader>
 
-      <SectionContainerMultiColumn>
+      <SectionContainerMultiColumn tone={tone_background}>
         {Array(numberOfItems)
           .fill(null)
           .map((_, index) => (
-            <LoadingInput key={index} />
+            <LoadingInput tone={tone_elements} key={index} />
           ))}
       </SectionContainerMultiColumn>
     </Section>
