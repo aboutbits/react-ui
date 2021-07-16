6f75a6d2ab0a38b5566f5c44f60346d05fd2f62f
Moritz Mock <mock.moritz@rolmail.net>
2021-07-16 09:48:29 +0200

    LoadingList.tsx passes now tone for background and elements downwards

--- src/components/loading/LoadingList.tsx
+++ src/components/loading/LoadingList.tsx
@@ -10,7 +10,9 @@ import { LoadingBar } from '.'
 const LoadingList: React.FC<{
   numberOfItems: number
   sectionHeader: React.ReactNode
-}> = ({ numberOfItems, sectionHeader }) => {
+  tone_background?: string
+  tone_elements?: string
+}> = ({ numberOfItems, sectionHeader, tone_background, tone_elements }) => {
   return (
     <Section>
       <SectionHeader>
@@ -21,9 +23,9 @@ const LoadingList: React.FC<{
         {Array(numberOfItems)
           .fill(null)
           .map((_, index) => (
-            <SectionListItem key={index}>
-              <LoadingBar className="p-4 mr-4 w-full" />
-              <LoadingBar className="p-4 w-12" />
+            <SectionListItem tone={tone_background} key={index}>
+              <LoadingBar tone={tone_elements} className="p-4 mr-4 w-full" />
+              <LoadingBar tone={tone_elements} className="p-4 w-12" />
             </SectionListItem>
           ))}
       </SectionListContainer>
