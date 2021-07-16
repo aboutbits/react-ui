f135ca1e9ced2b551f0de19ef4c8ba82e09e8c81
Moritz Mock <mock.moritz@rolmail.net>
2021-07-16 14:49:11 +0200

    removed function

--- src/components/section/SectionItem.tsx
+++ src/components/section/SectionItem.tsx
@@ -1,14 +1,9 @@
-import classnames from 'classnames'
+import classNames from 'classnames'
 import { ReactNode } from 'react'
 
 export enum SectionDescriptionItemVariant {
   loaded = 'loaded',
   error = 'error',
-}
-
-const variantStyles: Record<SectionDescriptionItemVariant, string> = {
-  [SectionDescriptionItemVariant.loaded]: 'border-gray text-white',
-  [SectionDescriptionItemVariant.error]: 'border-critical text-critical',
 }
 
 export const SectionListItem: React.FC<{
@@ -17,7 +12,7 @@ export const SectionListItem: React.FC<{
 }> = ({ className, children, tone = 'gray-700' }) => {
   return (
     <div
-      className={classnames(
+      className={classNames(
         className,
         `flex items-center min-h-14 px-4 lg:px-5 text-white bg-${tone}`
       )}
@@ -31,12 +26,13 @@ export const SectionDescriptionItem: React.FC<{
   title: ReactNode
   content: ReactNode
   variant?: SectionDescriptionItemVariant
-}> = ({ title, content, variant = SectionDescriptionItemVariant.loaded }) => {
+  className?: string
+}> = ({ title, content, className }) => {
   return (
     <div
-      className={classnames(
-        variantStyles[variant],
-        'flex flex-col space-y-1 pb-2 border-b border-gray-300 text-white'
+      className={classNames(
+        'flex flex-col space-y-1 pb-2 border-b border-gray text-white',
+        className
       )}
     >
       <dt className="text-sm">{title}</dt>
