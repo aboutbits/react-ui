b54d09771f3b9319cdddfed32ce3f82877951cff
Moritz Mock <mock.moritz@rolmail.net>
2021-07-19 14:43:28 +0200

    simplified button story

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -8,45 +8,39 @@ export default {
   component: Button,
 } as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>Click Me!</Button>
+)
 
 export const Default = Template.bind({})
-Default.args = {
-  children: 'Click Me!',
-}
+Default.args = {}
 
 export const VarianSolid = Template.bind({})
 VarianSolid.args = {
-  children: 'Click Me!',
   variant: Variant.solid,
 }
 
 export const VariantGhost = Template.bind({})
 VariantGhost.args = {
-  children: 'Click Me!',
   variant: Variant.ghost,
 }
 
 export const VariantTransparent = Template.bind({})
 VariantTransparent.args = {
-  children: 'Click Me!',
   variant: Variant.transparent,
 }
 
 export const TonePrimary = Template.bind({})
 TonePrimary.args = {
-  children: 'Click Me!',
   tone: Tone.primary,
 }
 
 export const ToneCritical = Template.bind({})
 ToneCritical.args = {
-  children: 'Click Me!',
   tone: Tone.critical,
 }
 
 export const ToneCustom = Template.bind({})
 ToneCustom.args = {
-  children: 'Click Me!',
   tone: 'green',
 }
