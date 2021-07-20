1bb171dfda46780429252f08dc26f731b0d5e703
Moritz Mock <mock.moritz@rolmail.net>
2021-07-20 10:24:07 +0200

    removed default values for size, tone, and variant in ButtonWithIcon.tsx

--- src/components/button/ButtonWithIcon.tsx
+++ src/components/button/ButtonWithIcon.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonProps, Size, Variant, Tone } from './Button'
+import { Button, ButtonProps } from './Button'
 import React, { ComponentType } from 'react'
 import { IconProps } from '@aboutbits/react-material-icons/dist/types'
 import classNames from 'classnames'
@@ -11,23 +11,13 @@ type ButtonWithIconProps = ButtonProps & {
 }
 
 const ButtonWithIcon = React.forwardRef<HTMLButtonElement, ButtonWithIconProps>(
-  (
-    {
-      children,
-      Icon,
-      size = Size.md,
-      variant = Variant.solid,
-      tone = Tone.primary,
-      ...props
-    },
-    ref
-  ) => {
+  ({ children, Icon, ...props }, ref) => {
     return (
-      <Button size={size} tone={tone} variant={variant} {...props} ref={ref}>
+      <Button {...props} ref={ref}>
         <div className="flex justify-center items-center">
           <Icon
             className={classNames(
-              `mr-1 fill-current ${size === 'sm' ? 'w-3 h3' : 'w-4 h-4'}`
+              `mr-1 fill-current ${props.size === 'sm' ? 'w-3 h3' : 'w-4 h-4'}`
             )}
           />
           {children}
