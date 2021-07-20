4bb43ed7600477af5e67593bed34b57db273d4c7
Moritz Mock <mock.moritz@rolmail.net>
2021-07-20 09:43:57 +0200

    added default values for ButtonWithIcon

--- src/components/button/ButtonWithIcon.tsx
+++ src/components/button/ButtonWithIcon.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonProps } from './Button'
+import { Button, ButtonProps, Size, Variant, Tone } from './Button'
 import React, { ComponentType } from 'react'
 import { IconProps } from '@aboutbits/react-material-icons/dist/types'
 import classNames from 'classnames'
@@ -8,19 +8,26 @@ type ButtonWithIconProps = ButtonProps & {
    * Defines the icon of the button.
    */
   Icon: ComponentType<IconProps>
-
-  className?: string
 }
 
 const ButtonWithIcon = React.forwardRef<HTMLButtonElement, ButtonWithIconProps>(
-  ({ children, Icon, className, ...props }, ref) => {
+  (
+    {
+      children,
+      Icon,
+      size = Size.md,
+      variant = Variant.solid,
+      tone = Tone.primary,
+      ...props
+    },
+    ref
+  ) => {
     return (
-      <Button {...props} ref={ref}>
+      <Button size={size} tone={tone} variant={variant} {...props} ref={ref}>
         <div className="flex justify-center items-center">
           <Icon
             className={classNames(
-              `mr-1 fill-current ${props.size === 'sm' ? 'w-3 h3' : 'w-4 h-4'}`,
-              className
+              `mr-1 fill-current ${size === 'sm' ? 'w-3 h3' : 'w-4 h-4'}`
             )}
           />
           {children}
