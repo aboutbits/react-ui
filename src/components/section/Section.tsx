ca3751cf70134fa24795d71a8c3691195ed31d51
Moritz Mock <mock.moritz@rolmail.net>
2021-07-20 15:32:42 +0200

    copied section

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -5,13 +5,15 @@ export enum SectionBackground {
   gradient = 'gradient',
 }
 
+export type SectionProps = { background?: SectionBackground }
+
 const backgroundStyle: Record<SectionBackground, string> = {
   [SectionBackground.white]: 'bg-white',
   [SectionBackground.gradient]:
     'bg-gradient-to-b from-white to-primary-100 border-t border-gray-300',
 }
 
-export const Section: React.FC<{ background?: SectionBackground }> = ({
+export const Section: React.FC<SectionProps> = ({
   background = SectionBackground.white,
   children,
 }) => (
@@ -20,6 +22,30 @@ export const Section: React.FC<{ background?: SectionBackground }> = ({
   </section>
 )
 
+export const SectionContent: React.FC<{ className?: string }> = ({
+  children,
+  className,
+}) => (
+  <div className={classnames('pt-5 pb-10 px-4 lg:px-5 bg-gray-700', className)}>
+    {children}
+  </div>
+)
+
+export const SectionContentTwoColumn: React.FC<{
+  className?: string
+}> = ({ children, className = '' }) => (
+  <SectionContent
+    className={classnames('grid xl:grid-cols-2 xl:gap-x-5 gap-y-5', className)}
+  >
+    {children}
+  </SectionContent>
+)
+
+export const SectionContentList: React.FC = ({ children }) => (
+  <div className="space-y-px bg-gray-300">{children}</div>
+)
+
+//TODO: check were this file is now
 export const SectionContainerMultiColumn: React.FC<{
   className?: string
   tone?: string
@@ -29,8 +55,4 @@ export const SectionContainerMultiColumn: React.FC<{
   >
     {children}
   </div>
-)
-
-export const SectionListContainer: React.FC = ({ children }) => (
-  <div className="space-y-px bg-gray-300">{children}</div>
 )
