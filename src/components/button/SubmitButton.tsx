8eee51aabd95bbebe680033fef649caa08cb8ce0
Moritz Mock <mock.moritz@rolmail.net>
2021-07-20 10:25:49 +0200

    removed default values for SubmitButton.tsx added ref to SubmitButton.tsx

--- src/components/button/SubmitButton.tsx
+++ src/components/button/SubmitButton.tsx
@@ -1,40 +1,34 @@
 import { useFormikContext } from 'formik'
 import { FormattedMessage } from 'react-intl'
 
-import { Button, Size, Tone, Variant, ButtonProps } from './Button'
+import { Button, ButtonProps } from './Button'
 import React from 'react'
 
-export type SubmitButtonProps = ButtonProps & {
-  /**
-   * Defines the disabling status of a button.
-   */
-  disabled?: boolean
-}
+const SubmitButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children = <FormattedMessage id="shared.save" />,
+      disabled = false,
+      ...props
+    },
+    ref
+  ) => {
+    const formik = useFormikContext()
 
-const SubmitButton: React.FC<
-  SubmitButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>
-> = ({
-  children = <FormattedMessage id="shared.save" />,
-  variant = Variant.solid,
-  disabled = false,
-  tone = Tone.primary,
-  size = Size.md,
-  ...props
-}) => {
-  const formik = useFormikContext()
+    return (
+      <Button
+        type="submit"
+        disabled={formik?.isSubmitting || disabled}
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </Button>
+    )
+  }
+)
 
-  return (
-    <Button
-      type="submit"
-      variant={variant}
-      disabled={formik?.isSubmitting || disabled}
-      tone={tone}
-      size={size}
-      {...props}
-    >
-      {children}
-    </Button>
-  )
-}
+// This improves readability in dev tools
+SubmitButton.displayName = 'SubmitButton'
 
 export { SubmitButton }
