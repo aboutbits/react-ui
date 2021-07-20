1553a1189e81261a19dbb3c903d0ae1af274d7d1
Moritz Mock <mock.moritz@rolmail.net>
2021-07-20 15:39:48 +0200

    added SectionError

---
+++ src/components/section/SectionError.tsx
@@ -0,0 +1,32 @@
+import classnames from 'classnames'
+import styles from './SectionList.module.css'
+import { Warning } from '../svg/Warning'
+import { FormattedMessage } from 'react-intl'
+import { Button, Variant } from '../button/Button'
+
+const SectionError: React.FC = ({ children }) => (
+  <div
+    className={classnames(
+      'flex flex-col items-center justify-center overflow-hidden mx-auto my-15 lg:mt-20',
+      styles['error-width']
+    )}
+  >
+    <div className="w-60 h-60">
+      <Warning />
+    </div>
+    <div className="mt-7 text-4xl font-bold">
+      <FormattedMessage id="shared.error.title" />
+    </div>
+    <div className="mt-5 w-full text-xl text-center break-words">
+      {children}
+    </div>
+    <Button
+      variant={Variant.solid}
+      onClick={() => window.location.reload()}
+      className="mt-20 w-full"
+    >
+      <FormattedMessage id="shared.button.reload" />
+    </Button>
+  </div>
+)
+export { SectionError }
