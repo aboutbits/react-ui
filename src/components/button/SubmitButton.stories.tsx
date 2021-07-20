e83614cdc852f40748d54057f57c79fd17d4efe8
Moritz Mock <mock.moritz@rolmail.net>
2021-07-20 10:25:04 +0200

    removed not needed stories for the SubmitButton.tsx

--- src/components/button/SubmitButton.stories.tsx
+++ src/components/button/SubmitButton.stories.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import { SubmitButton } from './SubmitButton'
-import { Variant, Tone } from './Button'
 
 export default {
   title: 'Components/Button/Submit',
   component: SubmitButton,
 } as ComponentMeta<typeof SubmitButton>
 
+//TODO add formik in default
 const Template: ComponentStory<typeof SubmitButton> = (args) => (
   <SubmitButton {...args} />
 )
@@ -17,36 +17,6 @@ export const Default = Template.bind({})
 Default.args = {
   children: 'Submit!',
   disabled: false,
-}
-
-export const VariantSolid = Template.bind({})
-VariantSolid.args = {
-  children: 'Submit',
-  variant: Variant.solid,
-}
-
-export const VariantGhost = Template.bind({})
-VariantGhost.args = {
-  children: 'Submit!',
-  variant: Variant.ghost,
-}
-
-export const VariantTransparent = Template.bind({})
-VariantTransparent.args = {
-  children: 'Submit!',
-  variant: Variant.transparent,
-}
-
-export const TonePrime = Template.bind({})
-TonePrime.args = {
-  children: 'Submit!',
-  tone: Tone.primary,
-}
-
-export const ToneCustom = Template.bind({})
-ToneCustom.args = {
-  children: 'Submit!',
-  tone: 'green',
 }
 
 export const Disabled = Template.bind({})
