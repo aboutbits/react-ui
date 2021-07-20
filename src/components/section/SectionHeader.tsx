b3994ed8d817c72abaf4ddde60f10df3934fda7f
Moritz Mock <mock.moritz@rolmail.net>
2021-07-20 15:43:06 +0200

    copied SectionHeader.tsx

--- src/components/section/SectionHeader.tsx
+++ src/components/section/SectionHeader.tsx
@@ -1,21 +1,27 @@
-import classnames from 'classnames'
+import classNames from 'classnames'
+import { ReactNode } from 'react'
 
-const SectionHeader: React.FC<{ className?: string }> = ({
+export const SectionHeader: React.FC<{ className?: string }> = ({
   className,
   children,
 }) => (
-  <div className={classnames('px-4 lg:px-5 pt-5 pb-3 bg-white', className)}>
+  <div className={classNames('px-4 lg:px-5 pt-5 pb-3 bg-white', className)}>
     {children}
   </div>
 )
 
-const SectionTitle: React.FC<{ className?: string }> = ({
-  className,
-  children,
-}) => (
-  <h1 className={classnames(`text-xs font-bold uppercase`, className)}>
-    {children}
-  </h1>
+export const SectionTitle: React.FC = ({ children }) => (
+  <h1 className="text-xs font-bold uppercase">{children}</h1>
 )
 
-export { SectionHeader, SectionTitle }
+export const SectionHeaderWithAction: React.FC<{
+  title: ReactNode
+  actionIcon: ReactNode
+}> = ({ title, actionIcon }) => (
+  <SectionHeader>
+    <div className="flex justify-between items-center">
+      <SectionTitle>{title}</SectionTitle>
+      {actionIcon}
+    </div>
+  </SectionHeader>
+)
