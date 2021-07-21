a94802066d15cd480f23dad2c11b936884258c5a
Moritz Mock <mock.moritz@rolmail.net>
2021-07-21 12:48:53 +0200

    detailed information about dialog setup and css

--- src/components/dialog/confirmation/ConfirmDialog.stories.tsx
+++ src/components/dialog/confirmation/ConfirmDialog.stories.tsx
@@ -6,6 +6,32 @@ import { Button } from '../../../index'
 export default {
   title: 'Components/Dialog',
   component: ConfirmationDialogComponent,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'There are some requirements if the component is used:<br>' +
+          '<ol>\n' +
+          '<li>The package of @reach/dialog needs to be installed (`npm i @reach/dialog`)</li>\n' +
+          '<li>Add the following css snippet:</li>\n' +
+          '</ol>' +
+          '<p>:root {<br>' +
+          '  --reach-dialog: 1;<br>' +
+          '}<br>' +
+          '<br>' +
+          '[data-reach-dialog-overlay] {<br>' +
+          '  background:rgba(0, 0, 0, 0.36);<br>' +
+          '  @apply fixed;<br>' +
+          '  @apply inset-0;<br>' +
+          '  @apply overflow-auto;<br>' +
+          '}<br>' +
+          '<br>' +
+          '[data-reach-dialog-content] {<br>' +
+          '  outline: none;<br>' +
+          '}</p>',
+      },
+    },
+  },
 } as ComponentMeta<typeof ConfirmationDialogComponent>
 
 const Template: ComponentStory<typeof ConfirmationDialogComponent> = (args) => {
