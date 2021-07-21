bd3f5aa25851fd7a256585be41f726a8cae366e5
Moritz Mock <mock.moritz@rolmail.net>
2021-07-21 11:21:24 +0200

    added stories for SectionContentTwoColumn.stories.tsx and SectionContentList.stories.tsx

---
+++ src/components/section/SectionContentList.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { SectionContentList as SectionContentListComponent } from './Section'
+import { Tone } from '../button/Button'
+
+export default {
+  title: 'Components/Section/SectionContentList',
+  component: SectionContentListComponent,
+} as ComponentMeta<typeof SectionContentListComponent>
+
+const Template: ComponentStory<typeof SectionContentListComponent> = (args) => (
+  <SectionContentListComponent {...args}>
+    I am a Section!
+  </SectionContentListComponent>
+)
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export const TonePrimary = Template.bind({})
+TonePrimary.args = {
+  tone: Tone.primary,
+}
