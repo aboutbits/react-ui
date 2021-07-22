1e8cd92262b177b05d8995016b8242cf0bf9b649
Moritz Mock <mock.moritz@rolmail.net>
2021-07-22 09:36:58 +0200

    fix description

--- src/components/section/type.ts
+++ src/components/section/type.ts
@@ -1,12 +1,12 @@
 export type ToneBackgroundProps = {
   /**
-   * Defines the tone of the background. Basically the color, so be sure to have it defined in Tailwind.
+   * Defines the tone of the background. Basically, the colour, so be sure to have it defined in Tailwind.
    * */
   tone?: string
 }
 export type ClassNameProps = {
   /**
-   * Adjusting individual the style with any CSS class.
+   * Adjusting individual style with any CSS class.
    * */
   className?: string
 }
