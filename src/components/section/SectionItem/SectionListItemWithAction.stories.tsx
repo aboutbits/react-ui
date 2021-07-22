93787cc73997be9ad67c3ba343b3341636cb0da2
Moritz Mock <mock.moritz@rolmail.net>
2021-07-22 12:20:16 +0200

    refactored SectionListItemWithAction.stories.tsx

--- src/components/section/SectionItem/SectionListItemWithAction.stories.tsx
+++ src/components/section/SectionItem/SectionListItemWithAction.stories.tsx
@@ -4,18 +4,35 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { SectionListItemWithAction as SectionListItemWithActionComponent } from './SectionItem'
 import { Tone } from '../../button/Button'
 import IconTimer from '@aboutbits/react-material-icons/dist/IconTimer'
+import { SectionContentList } from '../Section/SectionContentList'
 
 export default {
-  title: 'Components/Section/List Item With Action',
+  title: 'Components/Section/SectionListItemWithAction',
   component: SectionListItemWithActionComponent,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'This component adds an `SectionListItem` and applies basic styling.',
+      },
+    },
+  },
 } as ComponentMeta<typeof SectionListItemWithActionComponent>
 
 const Template: ComponentStory<typeof SectionListItemWithActionComponent> = (
   args
 ) => (
-  <SectionListItemWithActionComponent {...args}>
-    I am Section List Item With Action!
-  </SectionListItemWithActionComponent>
+  <SectionContentList>
+    <SectionListItemWithActionComponent {...args}>
+      Element 1
+    </SectionListItemWithActionComponent>
+    <SectionListItemWithActionComponent {...args}>
+      Element 2
+    </SectionListItemWithActionComponent>
+    <SectionListItemWithActionComponent {...args}>
+      Element 3
+    </SectionListItemWithActionComponent>
+  </SectionContentList>
 )
 
 export const Default = Template.bind({})
@@ -23,10 +40,12 @@ Default.args = {}
 
 export const TonePrimary = Template.bind({})
 TonePrimary.args = {
-  tone: Tone.primary,
+  backgroundColor: Tone.primary,
 }
 
 export const Icon = Template.bind({})
 Icon.args = {
-  actionIcon: <IconTimer width="24" height="24" />,
+  actionIcon: (
+    <IconTimer width="24" height="24" className="fill-current text-white" />
+  ),
 }
