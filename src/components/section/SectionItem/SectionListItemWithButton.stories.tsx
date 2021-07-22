33346a5c3474fe40a688f17f4437e82a2bd429bc
Moritz Mock <mock.moritz@rolmail.net>
2021-07-22 14:52:49 +0200

    refactored SectionListItemWithButton.stories.tsx

--- src/components/section/SectionItem/SectionListItemWithButton.stories.tsx
+++ src/components/section/SectionItem/SectionListItemWithButton.stories.tsx
@@ -2,24 +2,40 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import { SectionListItemWithButton as SectionListItemWithButtonComponent } from './SectionItem'
+import { SectionContentList } from '../Section/SectionContentList'
 
 export default {
-  title: 'Components/Section/List Item With Button',
+  title: 'Components/Section/SectionListItemWithButton',
   component: SectionListItemWithButtonComponent,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'This component adds an HTML-button element and applies basic styling.',
+      },
+    },
+  },
 } as ComponentMeta<typeof SectionListItemWithButtonComponent>
 
 const Template: ComponentStory<typeof SectionListItemWithButtonComponent> = (
   args
-) => (
-  <SectionListItemWithButtonComponent
-    {...args}
-    onClick={() => {
-      alert('Button was clicked!')
-    }}
-  >
-    I am a Section List Item With Button!
-  </SectionListItemWithButtonComponent>
-)
-
+) => {
+  const array = [1, 2, 3]
+  return (
+    <SectionContentList>
+      {array.map((elem) => (
+        <SectionListItemWithButtonComponent
+          {...args}
+          key={elem}
+          onClick={() => {
+            alert(`Button ${elem} was clicked!`)
+          }}
+        >
+          Button {elem}
+        </SectionListItemWithButtonComponent>
+      ))}
+    </SectionContentList>
+  )
+}
 export const Default = Template.bind({})
 Default.args = {}
