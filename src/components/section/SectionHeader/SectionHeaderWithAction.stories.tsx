91658fdd4fa8fcf1c28a4eb3ad769a694e0c2a2d
Moritz Mock <mock.moritz@rolmail.net>
2021-07-22 11:41:06 +0200

    refactored SectionHeaderWithAction.stories.tsx

--- src/components/section/SectionHeader/SectionHeaderWithAction.stories.tsx
+++ src/components/section/SectionHeader/SectionHeaderWithAction.stories.tsx
@@ -5,23 +5,28 @@ import { SectionHeaderWithAction as SectionHeaderWithActionComponent } from './S
 import IconAccessTime from '@aboutbits/react-material-icons/dist/IconAccessTime'
 
 export default {
-  title: 'Components/Section/Header With Action',
+  title: 'Components/Section/SectionHeaderWithAction',
   component: SectionHeaderWithActionComponent,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'This component adds an `SectionHeader` with a `SectionTile` and an `Icon` and applies basic positioning. <br> ',
+      },
+    },
+  },
 } as ComponentMeta<typeof SectionHeaderWithActionComponent>
 
 const Template: ComponentStory<typeof SectionHeaderWithActionComponent> = (
   args
 ) => (
-  <SectionHeaderWithActionComponent
-    {...args}
-    title={'I am a section header with action!'}
-  />
+  <SectionHeaderWithActionComponent {...args} title={'Profile information'} />
 )
 
 export const Default = Template.bind({})
 Default.args = {}
 
-export const Action = Template.bind({})
-Action.args = {
+export const Icon = Template.bind({})
+Icon.args = {
   actionIcon: <IconAccessTime width="24" height="24" />,
 }
