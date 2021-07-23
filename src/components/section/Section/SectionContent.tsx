c9627614e75ad1d31e99010b890283fae3f420b9
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-07-23 13:06:01 +0200

    inline types

--- src/components/section/Section/SectionContent.tsx
+++ src/components/section/Section/SectionContent.tsx
@@ -1,10 +1,13 @@
 import classNames from 'classnames'
-import { SectionProps } from './Section'
 
-export const SectionContent: React.FC<SectionProps> = ({
-  children,
-  className,
-}) => (
+type Props = {
+  /**
+   * Adjusting individual style with any CSS class.
+   * */
+  className?: string
+}
+
+export const SectionContent: React.FC<Props> = ({ children, className }) => (
   <div
     className={classNames(
       'pt-5 pb-10 px-4 lg:px-5 bg-section-content',
