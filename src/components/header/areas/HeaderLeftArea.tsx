21011c98f6ddb94f5eee099c0f0b4993a53319f5
Moritz Mock <mock.moritz@rolmail.net>
2021-07-27 07:54:41 +0200

    added ClassName props from types.ts
    added story for HeaderLeftArea

--- src/components/header/areas/HeaderLeftArea.tsx
+++ src/components/header/areas/HeaderLeftArea.tsx
@@ -1,6 +1,8 @@
-const HeaderLeftArea: React.FC<{
-  className?: string
-}> = ({ className, children }) => {
+import { ClassNameProps } from '../../types'
+
+type Props = ClassNameProps
+
+const HeaderLeftArea: React.FC<Props> = ({ className, children }) => {
   return (
     <div className={className}>
       <div className="flex items-center mr-4">{children}</div>
