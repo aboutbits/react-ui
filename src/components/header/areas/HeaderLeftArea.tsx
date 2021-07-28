4e9ce2612e20cebeb76f9ffd838be144828f9864
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-07-28 15:12:45 +0200

    Add multiple children example

--- src/components/header/areas/HeaderLeftArea.tsx
+++ src/components/header/areas/HeaderLeftArea.tsx
@@ -5,7 +5,9 @@ type Props = ClassNameProps
 const HeaderLeftArea: React.FC<Props> = ({ className, children }) => {
   return (
     <div className={className}>
-      <div className="flex items-center mr-4">{children}</div>
+      <div className="flex items-center mr-4 space-x-3 lg:space-x-4">
+        {children}
+      </div>
     </div>
   )
 }
