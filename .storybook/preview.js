d7c3a4a8cafbdcf295986f9a82b83675dd5c139f
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-07-29 17:57:46 +0200

    Refactor move IntlProvider in preview so that every story is wrapped with IntlProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
+import React from 'react'
+import { IntlProvider } from 'react-intl';
 import '../styles/index.css'
+import { ThemeProvider } from '../src/theme/ThemeProvider'
+import { makeTheme } from '../src/theme/theme'
+import enMessages from '../src/translations/shared.en.json'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,3 +14,16 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={makeTheme()}>
+      <IntlProvider
+        locale="en"
+        messages={enMessages}
+      >
+        <Story />
+      </IntlProvider>
+    </ThemeProvider>
+  ),
+];
