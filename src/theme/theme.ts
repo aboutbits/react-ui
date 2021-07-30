b31de287d90e8fbbffc3eea9deb52ceb7aafece2
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-07-30 16:04:17 +0200

    wip

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -32,6 +32,32 @@ const defaultTheme = {
       normal: 'bg-gray',
     },
   },
+  alert: {
+    container: {
+      base: 'p-3',
+      tone: {
+        positive: 'border border-positive bg-positive-100',
+        critical: 'border border-critical bg-critical-100',
+      },
+    },
+    content: {
+      base: 'flex items-center space-x-3',
+    },
+    iconContainer: {
+      base: 'flex flex-shrink-0 justify-center items-center w-6 h-6 rounded-full',
+      tone: {
+        positive: 'bg-positive',
+        critical: 'bg-critical',
+      },
+    },
+    icon: {
+      base: 'w-4 h-4 fill-current',
+      tone: {
+        positive: 'text-white',
+        critical: 'text-white',
+      },
+    },
+  },
 }
 
 export function makeTheme(overrideTheme?: OverrideTheme): Theme {
