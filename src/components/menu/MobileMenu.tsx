acb62e4009eea840f505d52f083422e2687b6e30
Moritz Mock <mock.moritz@rolmail.net>
2021-08-09 11:14:18 +0200

    added accessibility label to MobileMenu adjusted theme description for MobileMenu story

--- src/components/menu/MobileMenu.tsx
+++ src/components/menu/MobileMenu.tsx
@@ -1,23 +1,30 @@
-import { useIntl } from 'react-intl'
 import { Dialog } from '@reach/dialog'
+import classNames from 'classnames'
 import { useMenuToggle, useMenuState } from '../header/areas/MenuContext'
 import { useTheme } from '../../designSystem/theme/ThemeContext'
+import { ClassNameProps } from '../types'
 import { MobileMenuCloseButton } from './MobileMenuCloseButton'
 
-const MobileMenu: React.FC = ({ children }) => {
-  const intl = useIntl()
+type MobileMenuProps = ClassNameProps & {
+  /**
+   * Define accessibility label for the dialog.
+   * */
+  dialogLabel: string
+}
+
+const MobileMenu: React.FC<MobileMenuProps> = ({
+  children,
+  className,
+  dialogLabel,
+}) => {
   const menuState = useMenuState()
   const menuToggle = useMenuToggle()
 
   const { menu } = useTheme()
 
   return (
-    <Dialog
-      isOpen={menuState}
-      onDismiss={menuToggle}
-      aria-label={intl.formatMessage({ id: 'app.nav.accessibility.main' })}
-    >
-      <div className={menu.mobile.base}>
+    <Dialog isOpen={menuState} onDismiss={menuToggle} aria-label={dialogLabel}>
+      <div className={classNames(menu.mobile.base, className)}>
         {children}
         <MobileMenuCloseButton />
       </div>
