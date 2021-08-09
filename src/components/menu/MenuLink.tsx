74f8183304216844366a2be027a5b70d9ff769d7
Moritz Mock <mock.moritz@rolmail.net>
2021-08-09 10:31:08 +0200

    refactored MenuLink

--- src/components/menu/MenuLink.tsx
+++ src/components/menu/MenuLink.tsx
@@ -1,4 +1,3 @@
-import { UrlObject } from 'url'
 import { IconProps } from '@aboutbits/react-material-icons/dist/types'
 import { ComponentType } from 'react'
 import classNames from 'classnames'
@@ -6,13 +5,11 @@ import { useLinkComponent } from '../../designSystem/router/LinkComponentContext
 import { useTheme } from '../../designSystem/theme/ThemeContext'
 import { useRouter } from '../../designSystem/router/RouterContext'
 
-declare type Url = string | UrlObject
-
 export type MenuLinkProps = {
   text: string
   icon: ComponentType<IconProps>
   strict?: boolean
-  href: Url
+  href: string
 }
 
 const MenuLink: React.FC<MenuLinkProps> = ({
@@ -32,17 +29,16 @@ const MenuLink: React.FC<MenuLinkProps> = ({
 
   return (
     <li>
-      <LinkComponent {...props}>
-        <a
-          className={classNames(
-            menu.link.base,
-            menu.link.normal,
-            active ? menu.link.active : menu.link.inactive
-          )}
-        >
-          <Icon className={menu.link.icon.base} />
-          <div className="flex-1">{text}</div>
-        </a>
+      <LinkComponent
+        className={classNames(
+          menu.link.base,
+          menu.link.normal,
+          active ? menu.link.active : menu.link.inactive
+        )}
+        {...props}
+      >
+        <Icon className={menu.link.icon.base} />
+        <div className="flex-1">{text}</div>
       </LinkComponent>
     </li>
   )
