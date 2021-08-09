7df469539680af5e46ab9bcdf487a5d5e801e4de
Moritz Mock <mock.moritz@rolmail.net>
2021-08-09 11:22:18 +0200

    adjusted MobileMenuCloseButton added MobileMenuCloseButton story

--- src/components/menu/MobileMenuCloseButton.tsx
+++ src/components/menu/MobileMenuCloseButton.tsx
@@ -1,12 +1,17 @@
 import IconClose from '@aboutbits/react-material-icons/dist/IconClose'
+import classNames from 'classnames'
 import { useMenuToggle } from '../header/areas/MenuContext'
 import { useTheme } from '../../designSystem/theme/ThemeContext'
+import { ClassNameProps } from '../types'
 
-const MobileMenuCloseButton: React.FC = () => {
+const MobileMenuCloseButton: React.FC<ClassNameProps> = ({ className }) => {
   const menuToggle = useMenuToggle()
   const { menu } = useTheme()
   return (
-    <button className={menu.mobile.closeButton.base} onClick={menuToggle}>
+    <button
+      className={classNames(menu.mobile.closeButton.base, className)}
+      onClick={menuToggle}
+    >
       <IconClose
         className={menu.mobile.closeButton.icon.base}
         width="24"
