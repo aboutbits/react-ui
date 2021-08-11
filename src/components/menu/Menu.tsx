25a94ca9423a6e49f3eeb589898c28fa99fa3a4e
Moritz Mock <mock.moritz@rolmail.net>
2021-08-11 10:16:48 +0200

    renamed text to menuButtonContent changed MenuButtonContent to ReactNode changed the span to a div

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,6 +1,7 @@
 import IconArrowDropUp from '@aboutbits/react-material-icons/dist/IconArrowDropUp'
 import { Menu as MenuReach, MenuButton, MenuList } from '@reach/menu-button'
 import classNames from 'classnames'
+import { ReactNode } from 'react'
 import { useTheme } from '../../designSystem/theme/ThemeContext'
 import { ClassNameProps } from '../types'
 
@@ -10,9 +11,9 @@ type MenuProps = ClassNameProps & {
    * */
   menuLabel: string
   /**
-   * Defines the text to the left of the arrow button.
+   * Defines the content to the left of the arrow button.
    * */
-  text: string
+  menuButtonContent: ReactNode
   /**
    * Defines the id attribute for the menu button.
    * */
@@ -22,7 +23,7 @@ type MenuProps = ClassNameProps & {
 const Menu: React.FC<MenuProps> = ({
   menuLabel,
   className,
-  text,
+  menuButtonContent,
   children,
   menuButtonId,
 }) => {
@@ -37,14 +38,14 @@ const Menu: React.FC<MenuProps> = ({
             aria-label={menuLabel}
             className={classNames(menu.menuButton.base, className)}
           >
-            <span
+            <div
               className={classNames(
                 menu.menuButton.text.base,
                 menu.menuButton.text.normal
               )}
             >
-              {text}
-            </span>
+              {menuButtonContent}
+            </div>
             <span aria-hidden>
               <IconArrowDropUp
                 className={classNames(
