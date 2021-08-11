ad2c1122f5c9cd0f17d51cb26dcd2ad4a82f07ac
Moritz Mock <mock.moritz@rolmail.net>
2021-08-11 11:03:40 +0200

    Revert "export InputProps"
    This reverts commit 4c8b04eb71a4e582b1220154a40a8a5948022fd1.

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -4,7 +4,7 @@ import { InputError } from './InputError'
 import { InputLabel } from './InputLabel'
 import { useCustomInputCss } from './useCustomInputCss'
 
-export type InputProps = {
+type Props = {
   /**
    * Defines the htmlFor for the label.
    * */
@@ -23,7 +23,7 @@ export type InputProps = {
   disabled?: boolean
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
+export const Input = forwardRef<HTMLInputElement, Props>(
   ({ label, id, name, disabled, ...props }, ref) => {
     const customCss = useCustomInputCss(name, disabled)
     const [field] = useField(name)
