9d23a90aa2523e6918e7bd47757e6c56a6f684f4
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-08-15 19:20:33 +0200

    test tree shaking

--- src/button.ts
+++ src/button.ts
@@ -1,2 +1,4 @@
-export { Button, Variant, Size, Tone } from './components/button/Button'
-export { ButtonWithIcon } from './components/button/ButtonWithIcon'
+import { Button, Variant, Size, Tone } from './components/button/Button'
+import { ButtonWithIcon } from './components/button/ButtonWithIcon'
+
+export { Button, Variant, Size, Tone, ButtonWithIcon }
