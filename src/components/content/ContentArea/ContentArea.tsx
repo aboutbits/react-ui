36249f2ed90d4f7d81d984b5a8457cbe34628d83
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-08-16 10:55:44 +0200

    add missing files to git

---
+++ src/components/content/ContentArea/ContentArea.tsx
@@ -0,0 +1,8 @@
+import { useTheme } from '../../../designSystem/theme/ThemeContext'
+
+const ContentArea: React.FC = ({ children }) => {
+  const { content } = useTheme()
+  return <div className={content.area.base}>{children}</div>
+}
+
+export { ContentArea }
---
+++ src/components/content/ContentError/ContentError.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react'
+
+import { Button } from '../../button'
+import { useTheme } from '../../../designSystem/theme/ThemeContext'
+import { useInternationalization } from '../../../designSystem/internationalization/InternationalizationContext'
+
+type Props = {
+  /**
+   * Warning icon
+   */
+  icon: ReactNode
+}
+
+export const ContentError: React.FC<Props> = ({ icon, children }) => {
+  const { content } = useTheme()
+  const internationalization = useInternationalization()
+  return (
+    <div className={content.error.base}>
+      <div className={content.error.icon.base}>{icon}</div>
+      <div className={content.error.title.base}>
+        {internationalization.translate('shared.error.title')}
+      </div>
+      <div className={content.error.children.base}>{children}</div>
+      <Button
+        onClick={() => window.location.reload()}
+        className={content.error.button.base}
+      >
+        {internationalization.translate('shared.button.reload')}
+      </Button>
+    </div>
+  )
+}
