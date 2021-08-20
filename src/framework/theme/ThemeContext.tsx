a9858ea55dff09646381e18c4f21aee026f737a9
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-08-20 11:09:37 +0200

    don't set default value in theme context, as it doesn't update properly with override values

--- src/framework/theme/ThemeContext.tsx
+++ src/framework/theme/ThemeContext.tsx
@@ -1,12 +1,12 @@
 import { createContext, useContext } from 'react'
-import { defaultTheme, Theme } from './theme'
+import { Theme } from './theme'
 
-export const ThemeContext = createContext<Theme>(defaultTheme)
+export const ThemeContext = createContext<Theme | undefined>(undefined)
 
 export function useTheme(): Theme {
   const theme = useContext(ThemeContext)
 
-  if (theme === null) {
+  if (theme === null || theme === undefined) {
     throw new Error('No theme available on context')
   }
 
