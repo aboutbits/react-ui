2ab2d12494e63775d55aa0162e9fe11f8c98a2b1
Moritz Mock <mock.moritz@rolmail.net>
2021-08-25 14:45:13 +0200

    removed label, since it was not working removed a gitignore

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -8,23 +8,17 @@ type MenuItemProps = ClassNameProps & {
    * Defines which action should be executed on clicking.
    * */
   onClick: () => void
-  /**
-   * Define accessibility label for the menu item.
-   * */
-  menuItemLabel: string
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({
   children,
   className,
   onClick,
-  menuItemLabel,
 }) => {
   const { menu } = useTheme()
 
   return (
     <ReachMenuItem
-      aria-label={menuItemLabel}
       className={classNames(
         menu.menuItem.base,
         menu.menuItem.normal,
