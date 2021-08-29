781c9e6916845667cd370f27a63cd6f1bad3fb4e
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-08-29 09:56:53 +0200

    improve navigation design

--- src/framework/theme/theme.ts
+++ src/framework/theme/theme.ts
@@ -327,20 +327,21 @@ export const defaultTheme = {
   },
   navigation: {
     navigation: {
-      base: 'flex flex-col flex-1',
+      base: 'flex flex-col flex-1 space-y-1',
     },
     item: {
-      base: 'flex flex-row items-center w-full py-3 lg:py-4 px-4 lg:px-5 outline-none',
-      normal: 'text-gray-700 hover:bg-gray-100 focus:bg-primary-50',
+      base: 'flex flex-row items-center w-full py-2 lg:py-3 px-4 lg:px-5 outline-none',
+      normal:
+        'text-gray-800 hover:text-black hover:bg-gray-100 focus:text-black focus:bg-gray-100 focus:ring',
       disabled: 'text-gray',
-      active: 'text-white from-primary bg-gradient-to-r to-primary-700',
+      active: 'text-primary bg-primary-50',
       icon: { base: 'mr-4 w-6 h-6 fill-current' },
       content: { base: 'flex-1 text-left' },
     },
     mobile: {
       dialog: {
         base: 'flex flex-row items-stretch min-h-screen relative mr-14 w-full max-w-sm',
-        normal: 'bg-gradient-to-b from-primary-100 to-white',
+        normal: 'bg-white',
         closeButton: {
           base: 'absolute right-0 p-4',
           normal: 'hover:opacity-60 active:opacity-60',
