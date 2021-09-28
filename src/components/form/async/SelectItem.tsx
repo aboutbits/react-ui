6bff3a25b6b65751c1e026690684879b18746990
Moritz Mock <mock.moritz@rolmail.net>
2021-09-28 09:01:02 +0200

    added HTMLButtonElement to SelectItem

--- src/components/form/async/SelectItem.tsx
+++ src/components/form/async/SelectItem.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react'
+import React, { useState } from 'react'
 import { useField } from 'formik'
 import classnames from 'classnames'
 import IconKeyboardArrowDown from '@aboutbits/react-material-icons/dist/IconKeyboardArrowDown'
@@ -13,7 +13,10 @@ import {
   Props as DialogProps,
 } from './SelectItemDialogWithSearch'
 
-type Props<ItemType extends ReferenceObject, Error> = {
+type Props<ItemType extends ReferenceObject, Error> = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
   id: string
   name: string
   label: string
@@ -21,14 +24,14 @@ type Props<ItemType extends ReferenceObject, Error> = {
   disabled?: boolean
   defaultValue: ItemType
 } & Pick<
-  DialogProps<ItemType, Error>,
-  | 'useGetData'
-  | 'dialogTitle'
-  | 'dialogLabel'
-  | 'noSearchResults'
-  | 'renderListItem'
-  | 'renderErrorMessage'
->
+    DialogProps<ItemType, Error>,
+    | 'useGetData'
+    | 'dialogTitle'
+    | 'dialogLabel'
+    | 'noSearchResults'
+    | 'renderListItem'
+    | 'renderErrorMessage'
+  >
 
 export type ReferenceObject = {
   id: string | number
@@ -36,97 +39,112 @@ export type ReferenceObject = {
   label?: string
 }
 
-export function SelectItem<ItemType extends ReferenceObject, Error>({
-  disabled = false,
-  id,
-  name,
-  label,
-  placeholder,
-  defaultValue,
-  useGetData,
-  dialogTitle,
-  dialogLabel,
-  noSearchResults,
-  renderListItem,
-  renderErrorMessage,
-}: Props<ItemType, Error>): ReactElement {
-  const [field, , helpers] = useField<ItemType>(name)
-  const [, , helpersId] = useField<ItemType>(name + '.id')
-  const [showDialog, setShowDialog] = useState<boolean>(false)
-  const customCss = useCustomInputCss(`${field.name}.id`, disabled)
-  const internationalization = useInternationalization()
-  return (
-    <>
-      <div>
-        <InputLabel inputId={id} label={label} className={customCss.labelCss} />
-        {field.value.id === '' || field.value.id === 0 ? (
-          <button
-            type="button"
-            id={id}
-            onClick={() => {
-              setShowDialog(true)
-            }}
-            className={classnames(
-              customCss.inputCss,
-              'flex flex-row text-left text-gray-700'
-            )}
-          >
-            <span className="flex-1">{placeholder}</span>
-            <IconKeyboardArrowDown className="w-6 h-6" />
-          </button>
-        ) : (
-          <div
-            className={classnames(
-              customCss.inputCss,
-              'flex flex-row text-left'
-            )}
-          >
+type ItemType = ReferenceObject
+
+const SelectItem = React.forwardRef<HTMLButtonElement, Props<ItemType, Error>>(
+  (
+    {
+      disabled = false,
+      id,
+      name,
+      label,
+      placeholder,
+      defaultValue,
+      useGetData,
+      dialogTitle,
+      dialogLabel,
+      noSearchResults,
+      renderListItem,
+      renderErrorMessage,
+    },
+    ref
+  ) => {
+    const [field, , helpers] = useField<ItemType>(name)
+    const [, , helpersId] = useField<ItemType>(name + '.id')
+    const [showDialog, setShowDialog] = useState<boolean>(false)
+    const customCss = useCustomInputCss(`${field.name}.id`, disabled)
+    const internationalization = useInternationalization()
+    return (
+      <>
+        <div>
+          <InputLabel
+            inputId={id}
+            label={label}
+            className={customCss.labelCss}
+          />
+          {field.value.id === '' || field.value.id === 0 ? (
             <button
               type="button"
               id={id}
-              onClick={() => setShowDialog(true)}
-              className="flex-1 text-left"
-            >
-              <span>{field.value.name}</span>
-            </button>
-            <button
-              type="button"
               onClick={() => {
-                helpers.setTouched(true)
-                helpersId.setTouched(true)
-                helpers.setValue(defaultValue)
+                setShowDialog(true)
               }}
-              className="pl-2"
+              className={classnames(
+                customCss.inputCss,
+                'flex flex-row text-left text-gray-700'
+              )}
+              {...ref}
             >
-              <IconClose
-                className="w-6 h-6"
-                title={internationalization.translate('shared.select.clear')}
-              />
+              <span className="flex-1">{placeholder}</span>
+              <IconKeyboardArrowDown className="w-6 h-6" />
             </button>
-          </div>
+          ) : (
+            <div
+              className={classnames(
+                customCss.inputCss,
+                'flex flex-row text-left'
+              )}
+            >
+              <button
+                type="button"
+                id={id}
+                onClick={() => setShowDialog(true)}
+                className="flex-1 text-left"
+                {...ref}
+              >
+                <span>{field.value.name}</span>
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  helpers.setTouched(true)
+                  helpersId.setTouched(true)
+                  helpers.setValue(defaultValue)
+                }}
+                className="pl-2"
+              >
+                <IconClose
+                  className="w-6 h-6"
+                  title={internationalization.translate('shared.select.clear')}
+                />
+              </button>
+            </div>
+          )}
+          <InputError name={field.name + '.id'} className="mt-1" />
+        </div>
+        {showDialog && (
+          <SelectItemDialogWithSearch
+            onDismiss={() => {
+              helpersId.setTouched(true)
+              setShowDialog(false)
+            }}
+            isOpen={showDialog}
+            onConfirm={(item: ItemType) => {
+              helpersId.setTouched(true)
+              helpers.setValue(item)
+              setShowDialog(false)
+            }}
+            useGetData={useGetData}
+            renderListItem={renderListItem}
+            renderErrorMessage={renderErrorMessage}
+            dialogTitle={dialogTitle}
+            dialogLabel={dialogLabel}
+            noSearchResults={noSearchResults}
+          />
         )}
-        <InputError name={field.name + '.id'} className="mt-1" />
-      </div>
-      {showDialog && (
-        <SelectItemDialogWithSearch
-          onDismiss={() => {
-            helpersId.setTouched(true)
-            setShowDialog(false)
-          }}
-          isOpen={showDialog}
-          onConfirm={(item: ItemType) => {
-            helpersId.setTouched(true)
-            helpers.setValue(item)
-            setShowDialog(false)
-          }}
-          useGetData={useGetData}
-          renderListItem={renderListItem}
-          renderErrorMessage={renderErrorMessage}
-          dialogTitle={dialogTitle}
-          dialogLabel={dialogLabel}
-          noSearchResults={noSearchResults}
-        />
-      )}
-    </>
-  )
-}
+      </>
+    )
+  }
+)
+
+SelectItem.displayName = 'SelectItem'
