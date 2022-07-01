d6737da680d1af1f3993e6b3270ae31f14e776a5
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-07-01 11:24:50 +0200

    add auto-submit component

---
+++ src/components/utils/useDebounce.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+export function useDebounce<T>(data: T, interval: number): T {
+  const [liveData, setLiveData] = useState<T>(data)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handler = window.setTimeout(() => {
+        setLiveData(data)
+      }, interval)
+      return () => {
+        window.clearTimeout(handler)
+      }
+    }
+  }, [data, interval])
+
+  return liveData
+}
