328d62b3785f1544835e8facacf094de61ffe3b5
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-07-29 10:32:41 +0200

    add comments to section-search types

--- src/components/section/SectionHeader/SectionSearch.tsx
+++ src/components/section/SectionHeader/SectionSearch.tsx
@@ -3,10 +3,13 @@ import { ReactElement } from 'react'
 import { useInternationalization } from '../../../framework'
 import { Input, InputProps, Variant } from '../../form'
 
+// Utility function to exclude props from a union type
 type ExcludeProp<Type, Fields> = {
   [Property in keyof Type as Exclude<Property, Fields>]: Type[Property]
 }
 
+// Exclude ref to avoid TypeScript error. Should be fixed in the future.
+// Make name optional to use 'search' as default
 export type SectionSearchProps = ExcludeProp<InputProps, 'ref' | 'name'> &
   Partial<Pick<InputProps, 'name'>>
 
