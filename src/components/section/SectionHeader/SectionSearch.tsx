dca0d640c0a9be3223bd720cadae5ad6eba2ccc5
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-07-29 10:29:15 +0200

    fix section-search to exclude ref and make name optional

--- src/components/section/SectionHeader/SectionSearch.tsx
+++ src/components/section/SectionHeader/SectionSearch.tsx
@@ -3,15 +3,22 @@ import { ReactElement } from 'react'
 import { useInternationalization } from '../../../framework'
 import { Input, InputProps, Variant } from '../../form'
 
-export type SectionSearchProps = Omit<InputProps, 'ref' | 'name'> &
+type ExcludeProp<Type, Fields> = {
+  [Property in keyof Type as Exclude<Property, Fields>]: Type[Property]
+}
+
+export type SectionSearchProps = ExcludeProp<InputProps, 'ref' | 'name'> &
   Partial<Pick<InputProps, 'name'>>
 
-export function SectionSearch({ ...props }: SectionSearchProps): ReactElement {
+export function SectionSearch({
+  name = 'search',
+  ...props
+}: SectionSearchProps): ReactElement {
   const internationalization = useInternationalization()
 
   return (
     <Input
-      name="search"
+      name={name}
       placeholder={internationalization.translate('shared.search.placeholder')}
       variant={Variant.soft}
       iconStart={IconSearch}
