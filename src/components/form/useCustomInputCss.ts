3df73cabf8891a6be47639feeca3f177bbedcfaf
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-08-17 16:43:51 +0200

    add toggle-switch component

--- src/components/form/useCustomInputCss.ts
+++ src/components/form/useCustomInputCss.ts
@@ -1,42 +1,73 @@
-import { useField } from 'formik'
-import { useTheme } from '../../framework'
+import { FieldMetaProps, useField } from 'formik'
+import { Theme, useTheme } from '../../framework'
 import { Mode } from '../types'
 import { Variant } from './types'
 
 const useCustomInputCss = (
-  fieldName: string,
-  fieldDisabled = false,
-  fieldMode = Mode.light,
+  name: string,
+  disabled = false,
+  mode = Mode.light,
   variant = Variant.ghost
 ): { inputCss: string; labelCss: string; errorCss: string } => {
-  const [, meta] = useField({ name: fieldName })
+  const [, meta] = useField({ name: name })
   const {
     form: { input, inputLabel, inputError },
   } = useTheme()
 
-  const inputModeCss = input.modeVariant[fieldMode]
-
-  let customInputCss =
-    inputModeCss.base.normal + ' ' + inputModeCss[variant].normal
-  let customLabelCss = inputLabel[fieldMode].normal
-  const customErrorCss = inputError[fieldMode].normal
-
-  if (meta.touched && meta.error) {
-    customInputCss = inputModeCss.base.error + ' ' + inputModeCss[variant].error
-    customLabelCss = inputLabel[fieldMode].error
-  }
-
-  if (fieldDisabled) {
-    customInputCss =
-      inputModeCss.base.disabled + ' ' + inputModeCss[variant].disabled
-    customLabelCss = inputLabel[fieldMode].disabled
-  }
+  const customInputCss = getCustomInputCss(input, meta, disabled, mode, variant)
+  const customLabelCss = getCustomLabelCss(inputLabel, meta, disabled, mode)
+  const customErrorCss = getCustomErrorCss(inputLabel, mode)
 
   return {
     inputCss: input.base + ' ' + customInputCss,
     labelCss: inputLabel.base + ' ' + customLabelCss,
     errorCss: inputError.base + ' ' + customErrorCss,
   }
+}
+
+export function getCustomLabelCss(
+  theme: Theme['form']['inputLabel'],
+  meta: FieldMetaProps<unknown>,
+  disabled: boolean,
+  mode: Mode
+) {
+  if (disabled) {
+    return theme[mode].disabled
+  }
+
+  if (meta.touched && meta.error) {
+    return theme[mode].error
+  }
+
+  return theme[mode].normal
+}
+
+export function getCustomInputCss(
+  theme: Theme['form']['input'],
+  meta: FieldMetaProps<unknown>,
+  disabled: boolean,
+  mode: Mode,
+  variant: Variant
+) {
+  const modeCss = theme.modeVariant[mode]
+  const modeVariantCss = modeCss[variant]
+
+  if (disabled) {
+    return modeCss.base.disabled + ' ' + modeVariantCss.disabled
+  }
+
+  if (meta.touched && meta.error) {
+    return modeCss.base.error + ' ' + modeVariantCss.error
+  }
+
+  return modeCss.base.normal + ' ' + modeVariantCss.normal
+}
+
+export function getCustomErrorCss(
+  theme: Theme['form']['inputError'],
+  mode: Mode
+) {
+  return theme[mode].normal
 }
 
 export { useCustomInputCss }
