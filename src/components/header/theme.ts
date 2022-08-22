8473f8d83c37eab55e63c378a2900930615f30b3
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-08-22 08:34:14 +0200

    use custom min-h values for header-area styles

--- src/components/header/theme.ts
+++ src/components/header/theme.ts
@@ -4,7 +4,7 @@ export default {
     desktop: 'hidden lg:inline-block',
   },
   area: {
-    base: 'mb-6 flex min-h-10 items-center lg:min-h-15',
+    base: 'mb-6 flex min-h-[2.5rem] items-center lg:min-h-[3.75rem]',
   },
   leftArea: {
     base: 'flex items-center mr-2 lg:mr-3 space-x-3 lg:space-x-4',
