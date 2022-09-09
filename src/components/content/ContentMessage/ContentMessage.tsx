9402cef383f81a48fe690ff42f74ee430ec48095
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-09-09 16:17:51 +0200

    extract content-message from section-content-message

---
+++ src/components/content/ContentMessage/ContentMessage.tsx
@@ -0,0 +1,62 @@
+import classNames from 'classnames'
+import { ComponentType, ReactElement, ReactNode } from 'react'
+import { useTheme } from '../../../framework'
+import { IconProps } from '../../../svg/types'
+import { ClassNameProps, Tone } from '../../types'
+
+export type ContentMessageProps = ClassNameProps & {
+  icon?: ComponentType<IconProps>
+  tone?: Tone
+  title?: ReactNode | string
+  text?: ReactNode | string
+}
+
+export function ContentMessage({
+  className,
+  icon: Icon,
+  title,
+  text,
+  tone = Tone.primary,
+}: ContentMessageProps): ReactElement {
+  const { content } = useTheme()
+
+  return (
+    <div className={classNames(content.message.base, className)}>
+      {Icon && (
+        <div
+          className={classNames(
+            content.message.iconContainer.base,
+            content.message.iconContainer.tone[tone]
+          )}
+        >
+          <Icon
+            className={classNames(
+              content.message.icon.base,
+              content.message.icon.tone[tone]
+            )}
+          />
+        </div>
+      )}
+      {title && (
+        <div
+          className={classNames(
+            content.message.title.base,
+            content.message.title.tone[tone]
+          )}
+        >
+          {title}
+        </div>
+      )}
+      {text && (
+        <div
+          className={classNames(
+            content.message.text.base,
+            content.message.text.tone[tone]
+          )}
+        >
+          {text}
+        </div>
+      )}
+    </div>
+  )
+}
--- src/components/content/index.ts
+++ src/components/content/index.ts
@@ -1,2 +1,3 @@
 export * from './ContentArea/ContentArea'
 export * from './ContentError/ContentError'
+export * from './ContentMessage/ContentMessage'
