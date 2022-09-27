8f00ca2e6b6076b8e1d52f4f5faa14728fdd0996
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-09-27 12:56:16 +0200

    improve section components stories

--- src/components/section/SectionItem/SectionLoadingListItem.tsx
+++ src/components/section/SectionItem/SectionLoadingListItem.tsx
@@ -1,12 +1,19 @@
+import classNames from 'classnames'
 import { ReactElement } from 'react'
 import { useTheme } from '../../../framework'
 import { LoadingBar } from '../../loading'
 import { SectionListItem } from './SectionItem'
 
-export function SectionLoadingListItem(): ReactElement {
+export function SectionLoadingListItem({
+  className,
+}: {
+  className?: string
+}): ReactElement {
   const { section } = useTheme()
   return (
-    <SectionListItem>
+    <SectionListItem
+      className={classNames(section.loading.listItem.base, className)}
+    >
       <LoadingBar className={section.loading.listItem.start.base} />
       <LoadingBar className={section.loading.listItem.end.base} />
     </SectionListItem>
