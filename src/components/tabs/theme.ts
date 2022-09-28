1475f359a09d93947f6c2587d9fad2aa7eba7d6e
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-09-28 15:58:46 +0200

    remove obsolete class in tabs theme

--- src/components/tabs/theme.ts
+++ src/components/tabs/theme.ts
@@ -3,7 +3,7 @@ export default {
     base: 'flex gap-x-3 overflow-x-auto',
   },
   tab: {
-    base: 'flex items-center whitespace-nowrap rounded-full fill-current py-2 px-5 font-medium outline-1 -outline-offset-2 outline-primary-500 focus:outline',
+    base: 'flex items-center whitespace-nowrap rounded-full fill-current py-2 px-5 font-medium -outline-offset-2 outline-primary-500 focus:outline',
     normal: 'hover:bg-neutral-100',
     active: 'bg-primary-100',
   },
