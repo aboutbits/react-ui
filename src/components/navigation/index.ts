a7e14d8ac9658a2fdb59f89e1bf4e8e5b4abe098
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-09-28 14:39:29 +0200

    remove obsolete navigation components

--- src/components/navigation/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './MobileNavigationDialog'
-export * from './MobileNavigationDialogCloseButton'
-export * from './Navigation'
-export * from './NavigationContext'
-export * from './NavigationItemButton'
-export * from './NavigationItemLink'
