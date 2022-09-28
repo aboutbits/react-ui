96e0e6f2b7cacecbedc6b0c05da0d628b1061ed2
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-09-28 13:37:09 +0200

    replace gray color in pagination + navigation theme

--- src/components/navigation/theme.ts
+++ src/components/navigation/theme.ts
@@ -5,8 +5,8 @@ export default {
   item: {
     base: 'flex flex-row items-center w-full py-2 lg:py-3 px-4 lg:px-5 outline-none',
     normal:
-      'text-gray-800 hover:text-black hover:bg-gray-100 focus:text-black focus:bg-gray-100 focus:ring',
-    disabled: 'text-gray-500',
+      'text-neutral-800 hover:text-black hover:bg-neutral-100 focus:text-black focus:bg-neutral-100 focus:ring',
+    disabled: 'text-neutral-500',
     active: 'text-primary-500 bg-primary-50',
     icon: { base: 'mr-4 w-6 h-6 fill-current' },
     content: { base: 'flex-1 text-left' },
