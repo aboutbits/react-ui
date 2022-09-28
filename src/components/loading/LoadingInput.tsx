4a3d2f96e269aa0ecb3b4a777375da1b7577a437
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-09-28 16:56:00 +0200

    fix loading-input class-name

--- src/components/loading/LoadingInput.tsx
+++ src/components/loading/LoadingInput.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { ReactElement } from 'react'
 import { useTheme } from '../../framework'
 import { LoadingBar } from './LoadingBar'
@@ -9,7 +10,7 @@ export function LoadingInput({
 }): ReactElement {
   const { loading } = useTheme()
   return (
-    <div className={(loading.input.base, className)}>
+    <div className={classNames(loading.input.base, className)}>
       <LoadingBar className={loading.input.upper.base} />
       <LoadingBar className={loading.input.lower.base} />
     </div>
