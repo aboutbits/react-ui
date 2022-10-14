41a4fe8bdae545db3127926d29f59e6d09c6c8e2
Lukas Weiss <lukas.weiss@aboutbits.it>
2022-10-14 08:19:16 +0200

    add classname prop to select-item and dialog-header-with-close

--- src/components/dialog/DialogHeader/DialogHeaderWithClose.tsx
+++ src/components/dialog/DialogHeader/DialogHeaderWithClose.tsx
@@ -1,10 +1,11 @@
 import { MouseEventHandler, ReactElement, ReactNode } from 'react'
+import { ClassNameProps } from '../../types'
 import { DialogHeaderArea } from './DialogHeaderArea'
 import { DialogHeaderCloseAction } from './DialogHeaderCloseAction'
 import { DialogHeaderRow } from './DialogHeaderRow'
 import { DialogHeaderTitle } from './DialogHeaderTitle'
 
-export type DialogHeaderWithCloseProps = {
+export type DialogHeaderWithCloseProps = ClassNameProps & {
   title?: ReactNode
   onDismiss?: MouseEventHandler<HTMLButtonElement>
 }
@@ -12,9 +13,10 @@ export type DialogHeaderWithCloseProps = {
 export function DialogHeaderWithClose({
   title,
   onDismiss,
+  className,
 }: DialogHeaderWithCloseProps): ReactElement {
   return (
-    <DialogHeaderArea>
+    <DialogHeaderArea className={className}>
       <DialogHeaderRow>
         <DialogHeaderCloseAction onClick={onDismiss} />
         <DialogHeaderTitle>{title}</DialogHeaderTitle>
