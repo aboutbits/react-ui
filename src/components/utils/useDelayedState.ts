222d6e2d45e880b64d2fc92ea65518627c8dd958
Lukas Weiss <lukas.weiss@aboutbits.it>
2023-01-12 08:15:17 +0100

    fix use-delayed-state to not return a new function on every render

--- src/components/utils/useDelayedState.ts
+++ src/components/utils/useDelayedState.ts
@@ -1,13 +1,14 @@
-import { useRef, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
 export function useDelayedState<State>(
   readyState: State
 ): [State, (status: State, duration?: number) => void] {
-  const [state, setState] = useState<State>(readyState)
-
+  const readyStateRef = useRef<State>(readyState)
   const timeoutRef = useRef<NodeJS.Timeout | null>(null)
 
-  function setDelayedState(status: State, duration = 0) {
+  const [state, setState] = useState<State>(readyStateRef.current)
+
+  const setDelayedState = useCallback((status: State, duration = 0) => {
     if (timeoutRef.current !== null) {
       clearTimeout(timeoutRef.current)
       timeoutRef.current = null
@@ -15,12 +16,12 @@ export function useDelayedState<State>(
 
     if (duration > 0) {
       timeoutRef.current = setTimeout(() => {
-        setState(readyState)
+        setState(readyStateRef.current)
       }, duration)
     }
 
     setState(status)
-  }
+  }, [])
 
   return [state, setDelayedState]
 }
