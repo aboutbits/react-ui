0c5d47cfd8a115781aca98a93ae0401da86d0b4d
Lukas Weiss <lukas.weiss@aboutbits.it>
2023-01-12 08:17:31 +0100

    fix use-form-submit-state to work correctly with rhf states

--- src/components/form/useFormSubmitState.tsx
+++ src/components/form/useFormSubmitState.tsx
@@ -1,34 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useFormContext } from 'react-hook-form'
 import { useDelayedState } from '../utils/useDelayedState'
 
 export function useFormSubmitState(successDelay = 2000) {
   const {
-    formState: {
-      isSubmitting: formIsSubmitting,
-      isSubmitSuccessful: formIsSubmitSuccessful,
-    },
+    formState: { isSubmitting, isSubmitSuccessful },
   } = useFormContext()
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
   const [isSubmitted, setIsSubmitted] = useDelayedState(false)
 
   useEffect(() => {
-    setIsSubmitting(formIsSubmitting)
-  }, [formIsSubmitting])
-
-  useEffect(() => {
-    if (isSubmitting && formIsSubmitSuccessful) {
-      setIsSubmitting(false)
-      setIsSubmitted(true, successDelay)
+    if (!isSubmitting) {
+      if (isSubmitSuccessful) {
+        setIsSubmitted(true, successDelay)
+      } else {
+        setIsSubmitted(false)
+      }
     }
-  }, [
-    isSubmitting,
-    formIsSubmitting,
-    formIsSubmitSuccessful,
-    setIsSubmitted,
-    successDelay,
-  ])
+  }, [isSubmitting, isSubmitSuccessful, successDelay, setIsSubmitted])
 
   return { isSubmitted, isSubmitting }
 }
