dee8bdea9ef6728c1d9a5f6c5396263c8eecd91c
Lukas Weiss <lukas.weiss@aboutbits.it>
2023-01-16 12:31:48 +0100

    use icon-props for content-message instead of icon-style

--- src/components/content/ContentMessage/ContentMessage.tsx
+++ src/components/content/ContentMessage/ContentMessage.tsx
@@ -9,25 +9,24 @@ import { ContentMessageText } from './ContentMessageText'
 import { ContentMessageTitle } from './ContentMessageTitle'
 
 export type ContentMessageProps = ClassNameProps &
-  Partial<Pick<ContentMessageIconProps, 'icon' | 'iconStyle'>> & {
+  Partial<Pick<ContentMessageIconProps, 'icon'>> & {
     tone?: Tone
     title?: ReactNode
     text?: ReactNode
+    iconProps?: Partial<ContentMessageIconProps>
   }
 
 export function ContentMessage({
   className,
   icon,
-  iconStyle,
   title,
   text,
   tone = Tone.primary,
+  iconProps,
 }: ContentMessageProps): ReactElement {
   return (
     <ContentMessageContainer className={className}>
-      {icon && (
-        <ContentMessageIcon icon={icon} iconStyle={iconStyle} tone={tone} />
-      )}
+      {icon && <ContentMessageIcon icon={icon} tone={tone} {...iconProps} />}
       {title && <ContentMessageTitle tone={tone}>{title}</ContentMessageTitle>}
       {text && <ContentMessageText tone={tone}>{text}</ContentMessageText>}
     </ContentMessageContainer>
