96c951293ad0068a3b20318bbbd173ce3ae46e35
devgioele <gioele.devitti@aboutbits.it>
2023-09-08 11:53:58 +0200

    add SectionAppendix stories

---
+++ src/components/section/Section/SectionAppendix.stories.tsx
@@ -0,0 +1,60 @@
+import { Meta, StoryObj } from '@storybook/react'
+import {
+  Controls,
+  Description,
+  Primary,
+  Stories,
+  Subheading,
+  Title,
+} from '@storybook/blocks'
+import { Button, ButtonVariant } from '../../button'
+import { Tone } from '../../types'
+import { Theme } from '../../../../.storybook/components/Theme'
+import { SectionAppendix } from './SectionAppendix'
+import { Section } from './Section'
+import { SectionContainer } from './SectionContainer'
+import { SectionContent } from './SectionContent'
+
+const meta = {
+  title: 'components/section/SectionAppendix',
+  component: SectionAppendix,
+  args: {
+    children: (
+      <Button tone={Tone.Neutral} variant={ButtonVariant.Transparent}>
+        Button inside the appendix
+      </Button>
+    ),
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Section>
+          <SectionContainer>
+            <SectionContent>Section container</SectionContent>
+          </SectionContainer>
+          <Story />
+        </Section>
+      )
+    },
+  ],
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Description />
+          <Primary />
+          <Subheading>Props</Subheading>
+          <Controls />
+          <Theme component="section" items={['appendix']} />
+          <Stories />
+        </>
+      ),
+    },
+  },
+} satisfies Meta<typeof SectionAppendix>
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
