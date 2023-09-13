e97ccff2ff3000efe14a4cf422302940aefb9951
devgioele <gioele.devitti@aboutbits.it>
2023-09-13 13:05:54 +0200

    add border and gap to menu list

--- src/components/menu/theme.ts
+++ src/components/menu/theme.ts
@@ -5,7 +5,7 @@ const className = {
     active: 'bg-primary-100',
   },
   menuList: {
-    base: 'py-2 whitespace-nowrap min-w-32 rounded shadow focus:outline-none bg-white absolute',
+    base: 'py-2 flex flex-col items-center gap-1 whitespace-nowrap min-w-32 rounded border border-primary-500/10 shadow focus:outline-none bg-white absolute',
   },
 }
 
