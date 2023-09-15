c0986922960d25ad73ee247bd608f7c37cc55c6a
Matthäus Nössing <noessing.matthaeus@protonmail.com>
2023-09-15 09:15:41 +0200

    make select item form field fix height

---
+++ src/components/content/WithPlaceholder/WithPlaceholder.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react'
+
+export type WithPlaceholderProps = {
+  children: ReactNode
+  placeholder?: ReactNode
+}
+
+export function WithPlaceholder({
+                                  children,
+                                  placeholder = '-',
+                                }: WithPlaceholderProps) {
+  return (
+    <>
+      {typeof children === 'number'
+        ? isNaN(children)
+          ? placeholder
+          : children
+        : children || placeholder}
+    </>
+  )
+}
