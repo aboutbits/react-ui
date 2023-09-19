2b62f26c52893f538668e4e944267559c6bff280
Matthäus Nössing <noessing.matthaeus@protonmail.com>
2023-09-19 10:27:57 +0200

    change alert and actions to tsx instead of mdx

---
+++ src/components/action/Actions.stories.tsx
@@ -0,0 +1,70 @@
+import {
+  Controls,
+  Markdown,
+  Primary,
+  Stories,
+  Subheading,
+  Title,
+} from '@storybook/addon-docs'
+import { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonVariant } from '../button'
+import { Tone } from '../types'
+import { Actions } from './Actions'
+import { Theme } from '../../../.storybook/components'
+
+const children = {
+  options: ['One button', 'Two buttons'],
+  mapping: {
+    'One button': <Button>Save</Button>,
+    'Two buttons': (
+      <>
+        <Button tone={Tone.Success}>Save</Button>
+        <Button variant={ButtonVariant.Ghost} tone={Tone.Neutral}>
+          Cancel
+        </Button>
+      </>
+    ),
+  },
+  control: { type: 'select' },
+}
+
+const meta = {
+  component: Actions,
+  title: 'Components/Actions/Actions',
+  args: {
+    children: children.mapping['One button'],
+  },
+  argTypes: {
+    children,
+  },
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Markdown>
+            The `Actions` component positions children in a flex container. It
+            makes sure that there is space between each item and that they stack
+            on mobile. This is very handy in a content area or a dialog, where
+            you have two buttons next to each other.
+          </Markdown>
+          <Primary />
+          <Subheading>Props</Subheading>
+          <Controls />
+          <Theme component="action" />
+          <Stories />
+        </>
+      ),
+    },
+  },
+} satisfies Meta<typeof Actions>
+
+export default meta
+type Story = StoryObj<typeof Actions>
+
+export const Default: Story = {}
+export const MultipleChildren: Story = {
+  args: {
+    children: { ...children.mapping['Two buttons'] },
+  },
+}
---
+++ src/components/alert/Alert.stories.tsx
@@ -0,0 +1,252 @@
+import {
+  Controls,
+  Markdown,
+  Primary,
+  Stories,
+  Subheading,
+  Title,
+} from '@storybook/addon-docs'
+import { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonVariant } from '../button'
+import { Size, Tone } from '../types'
+import { Theme } from '../../../.storybook/components'
+import { Alert } from './Alert'
+import IconWarning from '@aboutbits/react-material-icons/dist/IconWarning'
+import IconCheck from '@aboutbits/react-material-icons/dist/IconCheck'
+import IconInfo from '@aboutbits/react-material-icons/dist/IconInfo'
+import { AlertActionsPosition } from './types'
+import {
+  AlertCritical,
+  AlertInformative,
+  AlertSuccess,
+  AlertWarning,
+} from './ConvenientAlerts'
+
+const icons = {
+  options: ['None', 'Warning', 'Check', 'Info'],
+  mapping: {
+    None: undefined,
+    Warning: IconWarning,
+    Check: IconCheck,
+    Info: IconInfo,
+  },
+  control: { type: 'select' },
+}
+const DissmissCriticalButton = (
+  <Button
+    variant={ButtonVariant.Transparent}
+    size={Size.Sm}
+    tone={Tone.Critical}
+  >
+    Dismiss
+  </Button>
+)
+
+const DissmissSuccessButton = (
+  <Button
+    variant={ButtonVariant.Transparent}
+    size={Size.Sm}
+    tone={Tone.Success}
+  >
+    Dismiss
+  </Button>
+)
+
+const YesNoCriticalButtons = (
+  <>
+    <Button
+      variant={ButtonVariant.Transparent}
+      size={Size.Sm}
+      tone={Tone.Critical}
+    >
+      Yes
+    </Button>
+    <Button
+      variant={ButtonVariant.Transparent}
+      size={Size.Sm}
+      tone={Tone.Critical}
+    >
+      No
+    </Button>
+  </>
+)
+
+const actions = {
+  options: ['None', 'DismissCritical', 'YesNoCritical'],
+  mapping: {
+    None: undefined,
+    DismissCritical: DissmissCriticalButton,
+    YesNoCritical: YesNoCriticalButtons,
+  },
+  control: { type: 'select' },
+}
+
+const meta = {
+  component: Alert,
+  title: 'Components/Alert/Alert',
+  args: {
+    children:
+      'This alert component can be used to highlight a message to the user.',
+    tone: Tone.Critical,
+    icon: IconWarning,
+  },
+  argTypes: {
+    title: { control: 'text' },
+    icon: icons,
+    actions: actions,
+  },
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Markdown>
+            This alert component can be used to highlight a message to the user.
+            It supports different types of tones. It is possible to specify an
+            icon, a title, actions and the position of the actions. The library
+            also provides convenient alert components with predefined tone and
+            icon.
+          </Markdown>
+          <Primary />
+          <Subheading>Props</Subheading>
+          <Controls />
+          <Theme component="alert" />
+          <Stories />
+        </>
+      ),
+    },
+  },
+} satisfies Meta<typeof Alert>
+
+export default meta
+type Story = StoryObj<typeof Alert>
+
+export const Default: Story = {}
+
+export const Tones: Story = {
+  args: {
+    icon: undefined,
+  },
+  render: (args) => (
+    <div className="flex flex-row space-x-4">
+      <Alert {...args} tone={Tone.Primary}>
+        Primary message
+      </Alert>
+      <Alert {...args} tone={Tone.Neutral}>
+        Neutral message
+      </Alert>
+      <Alert {...args} tone={Tone.Warning}>
+        Warning message
+      </Alert>
+      <Alert {...args} tone={Tone.Critical}>
+        Critical message
+      </Alert>
+      <Alert {...args} tone={Tone.Success}>
+        Success message
+      </Alert>
+      <Alert {...args} tone={Tone.Informative}>
+        Informative message
+      </Alert>
+    </div>
+  ),
+}
+
+export const ConvinientAlertComponents: Story = {
+  render: () => (
+    <div className="flex flex-row space-x-4">
+      <AlertSuccess>Success message</AlertSuccess>
+      <AlertWarning>Warning message</AlertWarning>
+      <AlertCritical>Critical message</AlertCritical>
+      <AlertInformative>Informative message</AlertInformative>
+    </div>
+  ),
+  parameters: {
+    controls: {
+      disable: true,
+    },
+  },
+}
+
+export const ActionsResponsive: Story = {
+  args: {
+    icon: IconCheck,
+    tone: Tone.Success,
+    actions: DissmissSuccessButton,
+    actionsPosition: AlertActionsPosition.Responsive,
+  },
+  render: (args) => (
+    <Alert {...args}>
+      Success message with actions <b>responsive</b>
+    </Alert>
+  ),
+  argTypes: {
+    children: { control: { disabled: true } },
+  },
+}
+
+export const ActionsFixedRight: Story = {
+  args: {
+    icon: IconCheck,
+    tone: Tone.Success,
+    actions: DissmissSuccessButton,
+    actionsPosition: AlertActionsPosition.FixedRight,
+  },
+  render: (args) => (
+    <Alert {...args}>
+      Success message with actions <b>fixed to the right</b>
+    </Alert>
+  ),
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+export const ActionsFixedBottom: Story = {
+  args: {
+    icon: IconCheck,
+    tone: Tone.Success,
+    actions: DissmissSuccessButton,
+    actionsPosition: AlertActionsPosition.FixedBottom,
+  },
+  render: (args) => (
+    <Alert {...args}>
+      Success message with actions <b>fixed to the right</b>
+    </Alert>
+  ),
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+export const VeryLongMessage: Story = {
+  args: {
+    icon: IconWarning,
+    tone: Tone.Critical,
+  },
+  render: (args) => (
+    <Alert {...args}>
+      Duis mauris ligula, aliquam id maximus at, aliquam ut dui. Morbi ac neque
+      mattis libero cursus lobortis quis ac urna. Nullam vel augue vitae lacus
+      fermentum ullamcorper eu eget sapien.
+    </Alert>
+  ),
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+export const WithTitle: Story = {
+  args: { title: 'This is the title' },
+}
