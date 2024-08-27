4424cc9ad4cd9bbe0a9b0b4280d7c456403b912b
Matthäus Nössing <matthaeus.noessing@aboutbits.it>
2024-08-27 11:54:49 +0200

    disable eslint ?? operator rule for with placeholder

--- src/components/content/WithPlaceholder/WithPlaceholder.tsx
+++ src/components/content/WithPlaceholder/WithPlaceholder.tsx
@@ -14,6 +14,7 @@ export function WithPlaceholder({
   placeholder = '-',
   children,
 }: WithPlaceholderProps) {
+  /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
   return (
     <>
       {typeof children === 'number'
