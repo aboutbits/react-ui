b7fc40e68822f0d0e69b18791c77311a20f69fca
Andreas Hufler <andreas.hufler@aboutbits.it>
2024-10-17 09:48:54 +0200

    allow for a right element in section header

--- src/components/section/SectionHeader/SectionHeader.tsx
+++ src/components/section/SectionHeader/SectionHeader.tsx
@@ -1,16 +1,21 @@
 import { ReactNode } from 'react'
 import { SectionHeaderArea } from './SectionHeaderArea'
 import { SectionHeaderTitle } from './SectionHeaderTitle'
+import { SectionHeaderRow, SectionHeaderRowLayout } from './SectionHeaderRow'
 
 export type SectionHeaderProps = {
   title: ReactNode
   className?: string
+  right?: ReactNode
 }
 
-export function SectionHeader({ className, title }: SectionHeaderProps) {
+export function SectionHeader({ className, title, right }: SectionHeaderProps) {
   return (
     <SectionHeaderArea className={className}>
-      <SectionHeaderTitle>{title}</SectionHeaderTitle>
+      <SectionHeaderRow layout={SectionHeaderRowLayout.SpaceBetween}>
+        <SectionHeaderTitle>{title}</SectionHeaderTitle>
+        {right}
+      </SectionHeaderRow>
     </SectionHeaderArea>
   )
 }
