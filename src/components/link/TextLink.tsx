b286a0003e90eb6b8e43985189f5f10ec1997d5d
Lukas Weiss <lukas.weiss@aboutbits.it>
2025-01-23 10:57:02 +0100

    add text-button component (#328)

--- src/components/link/TextLink.tsx
+++ src/components/link/TextLink.tsx
@@ -3,18 +3,17 @@ import { forwardRef } from 'react'
 import { LinkComponentProps, useLinkComponent, useTheme } from '../../framework'
 
 export const TextLink = forwardRef<HTMLAnchorElement, LinkComponentProps>(
-  function TextLink({ children, className, internal = true, ...props }, ref) {
+  function TextLink({ className, internal = true, ...props }, ref) {
     const LinkComponent = useLinkComponent()
     const { link } = useTheme()
+
     return (
       <LinkComponent
         {...props}
         internal={internal}
         ref={ref}
         className={classNames(className, link.textLink.base)}
-      >
-        {children}
-      </LinkComponent>
+      />
     )
   },
 )
